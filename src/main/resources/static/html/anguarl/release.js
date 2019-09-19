665dbb30e2be2947aa83a356cd0136c6a6ab7178
chenny <bbc123good@163.com>
2019-09-19 23:29:20 +0800

    信息发布

--- src/main/resources/static/html/anguarl/release.js
+++ src/main/resources/static/html/anguarl/release.js
@@ -1,11 +1,35 @@
 var app = angular.module('myApp', []);
-    app.controller('relCtrl', function ($scope, $http) {
+app.controller('relCtrl', function ($scope, $http) {
+
+    $scope.show_num_prv = 0;
+    $scope.show_num_next = 0;
     $scope.info = {
-        prv : '上一步',
-        next : '下一步'
+        prv: '上一步',
+        next: '下一步'
     }
 
-    $scope.show = function () {
-        alert(2);
+    $scope.prve = function () {
+        $scope.show_num_next -= 1;
+
+        if ($scope.show_num_next < 3) {
+            $scope.info.next = '下一步';
+        }
+    }
+
+    $scope.next = function () {
+        $scope.show_num_next += 1;
+
+        if($scope.show_num_next == 1){
+           if (!$scope.headline) {
+               // this.message('1111111111111');
+               // toastr.error('不能为空');
+           }
+
+        }
+
+
+        if ($scope.show_num_next >= 2) {
+            $scope.info.next = '发  布';
+        }
     }
 });
