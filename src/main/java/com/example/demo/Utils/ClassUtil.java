f372f25de62f687b36c7a29ad28252d5a7b374e6
chenny <bbc123good@163.com>
2019-10-09 14:39:27 +0800

    二维码 条码 生成 解析

--- src/main/java/com/example/demo/Utils/ClassUtil.java
+++ src/main/java/com/example/demo/Utils/ClassUtil.java
@@ -21,7 +21,7 @@ import java.util.Map;
  * @date 2019/9/26 12:07
  * @email bbc123good@163.com
  * @address http://106.12.38.131:8011
- * @describe 用于实现接口返回规范的类  所有接口返回值都由该类封装
+ * @describe 类属性封装集合
  */
 public class ClassUtil {
 
