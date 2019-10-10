2004aa234be3af6097cf6d00329c6578c502a6a1
chenny <bbc123good@163.com>
2019-10-10 14:35:32 +0800

    分组显示

--- src/main/resources/static/html/anguarl/ground.js
+++ src/main/resources/static/html/anguarl/ground.js
@@ -1,10 +1,16 @@
 var app = angular.module('myGroup', []).controller('groupCtrl', function ($scope, $http, $q) {
 
     $scope.groupList = [];
+    $scope.serviceItem = [];
 
     $scope.init = function () {
         $scope.groupList = getSessionObj('groupList');
         $scope.type = getSessionObj('type');
+        $scope.serviceItem = getServiceItem('/getClassField_fw');
+        $scope.prvList = getAddr(1);
+        $scope.cityList = getAddr(31);
+        $scope.disList = getAddr(388);
+        console.log($scope.serviceItem);
     }
 
     $scope.goInfo = function (id) {
@@ -17,6 +23,21 @@ var app = angular.module('myGroup', []).controller('groupCtrl', function ($scope
             history.go(0);
         }
         // setSessionObj()
+    }
+
+    //获取一级区域
+    function getAddr(pid) {
+        var url = '/api/addr/getInfos?pid=' + pid;
+        var msg = ajax_http(url, method_get, null);
+        return msg.data;
+    }
+
+    $scope.selectPrv = function () {
+        $scope.cityList = getAddr($scope.procode);
+    }
+
+    $scope.selectCity = function () {
+        $scope.disList = getAddr($scope.citycode);
     }
 
 });
