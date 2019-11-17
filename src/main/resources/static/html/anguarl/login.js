d0633e2bd028ab0bfc27ee12fa583d2f207928fb
chenny <bbc123good@163.com>
2019-11-17 10:47:54 +0800

    后台管理

--- src/main/resources/static/html/anguarl/login.js
+++ src/main/resources/static/html/anguarl/login.js
@@ -23,12 +23,14 @@ var app = angular.module('myLogin', []).controller('loginCtrl', function ($scope
         var msg = ajax_http_post_login(url, date);
         if (msg.code == 200) {
             setUser(msg.data);
+            lightyear.notify("登录成功,正在跳转....", 'success', 5000, 'mdi mdi-emoticon-happy', 'top', 'center');
             setTimeout(function () {
                 href('/home');
             },3000)
 
         } else {
-            msg_error("账户密码输入不匹配,请重新输入");
+            lightyear.notify(msg.message, 'danger', 5000, 'mdi mdi-emoticon-happy', 'top', 'center');
+            // msg_error("账户密码输入不匹配,请重新输入");
             setTimeout(function () {
                 history.go(0);
             },2000);
