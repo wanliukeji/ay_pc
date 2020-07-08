78a7889075f3b074bfd30677a83176503e7df1eb
ETF <bbc123good@163.com>
2020-07-08 12:16:15 +0800

    生成合同

---
+++ src/main/java/com/example/demo/entity/mk/MkRule.java
@@ -0,0 +1,29 @@
+package com.example.demo.entity.mk;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
+@Table(name = "mk_rule")
+public class MkRule implements Serializable {
+
+  private static final long serialVersionUID = -6775222679549090142L;
+
+  @Id
+  @TableId(type = IdType.AUTO)
+  @Column
+  private long id;
+  private String details;
+  private String userId;
+  private String title;
+  private Integer ftype;
+  private Date createDate;
+
+}
