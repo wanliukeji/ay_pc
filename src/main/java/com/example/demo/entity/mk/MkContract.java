3cc432706f2fcec21ff1418f520da1b15b3d52fd
ETF <bbc123good@163.com>
2020-07-08 12:16:36 +0800

    生成合同

---
+++ src/main/java/com/example/demo/entity/mk/MkContract.java
@@ -0,0 +1,41 @@
+package com.example.demo.entity.mk;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Data
+@Table(name = "mk_contract")
+public class MkContract implements Serializable {
+
+  private static final long serialVersionUID = -6775222679549090142L;
+
+  @Id
+  @TableId(type = IdType.AUTO)
+  @Column
+  private long id;
+  private String zTime;
+  private String startDate;
+  private String endDate;
+  private Date creatDate;
+  private String fuid;
+  private String zuid;
+  private String fileUrl;
+  private String addr;
+  private String fid;
+  private Integer del;
+  private Double area;
+  private String payDate;
+  private Integer payDay;
+  private BigDecimal amount;
+  private BigDecimal yamount;
+  private Integer startDay;
+
+}
